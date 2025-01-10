dd16f4278a8eea1e27ba73330e73ffcb0c199e32
Elilaw179 <sundayelisha179@gmail.com>
2025-01-10 14:12:31 +0100

    first commit

---
+++ scripts/getdates.js
@@ -0,0 +1,25 @@
+
+const yearElement = document.querySelector("#year");
+const lastModifiedElement = document.querySelector("#lastModified");
+
+const currentDate = new Date();
+
+const formattedDate = currentDate.toLocaleString("en-GB", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hourCycle: "h12", 
+});
+
+yearElement.innerHTML = `
+  <span class="highlight">
+    ${currentDate.getFullYear()} &copy; Elisha Sunday ✅ Software Engineer ✅ Nigeria
+  </span>`;
+
+lastModifiedElement.innerHTML = `
+  <span class="lastmodified">
+    Last Modification: ${formattedDate}
+  </span>`;
