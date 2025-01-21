d7c68fe50319744a779f82ea474fe9337a396b9b
Elilaw179 <sundayelisha179@gmail.com>
2025-01-21 14:05:24 +0100

    temple structure built

---
+++ scripts/temples.js
@@ -0,0 +1,7 @@
+let btnMenu = document.getElementById("btn-menu");
+let nav = document.querySelector("nav");
+let heading = document.getElementById("heading");
+
+btnMenu.addEventListener("click", () => {
+    nav.classList.toggle("vissible");
+  });
